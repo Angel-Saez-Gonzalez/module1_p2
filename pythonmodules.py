297809f821617089588f19beb1faf2dc10a94af8
angelsaez251 <angelsaez251@gmail.com>
2021-11-03 09:18:43 +0100

    yes

---
+++ pythonmodules.py
@@ -0,0 +1,16 @@
+import extra.iota as a
+import extra.good.alpha as b
+import extra.good.beta as c
+import extra.good.best.sigma as d
+import extra.good.best.tau as e
+import extra.ugly.omega as f
+import extra.ugly.psi as g
+
+
+print(a.FunI())
+print(b.FunA())
+print(c.FunB())
+print(d.FunS())
+print(e.FunT())
+print(f.FunO())
+print(g.FunP())
---
+++ test.py
@@ -0,0 +1,12 @@
+#import one or more modules from math
+from math import floor, ceil
+
+#Importar todo
+import math as m
+
+#Atributo con pi
+m.pi
+
+#funcion modulo math
+m.pow()
+
